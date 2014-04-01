d9378b848b6762d827042a1ff5197e59c6ff8f77
Colin Shea <colin.shea@evogi.com>
2014-03-31 18:41:19 -0700

    Manage the master, set up repositories

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,4 +2,7 @@ file { '/etc/motd':
   content => "Welcome to your Vagrant-built virtual machine! Managed by Puppet."
 }
 
+class{'puppet::repo::puppetlabs': }
+Class['puppet::repo::puppetlabs'] -> Package <| |>
+
 import 'nodes/*.pp'
--- manifests/nodes/master.pp
+++ manifests/nodes/master.pp
@@ -1,15 +1,21 @@
 node 'puppetmaster' {
+  class { 'puppet::master': }
+
+  $puppetdb_host = 'localhost'
+  $puppetdb_port = 8080
+
   class { 'puppetdb':
     disable_ssl    => true,
-    listen_address => 'localhost',
-    listen_port    => 8080
+    listen_address => $puppetdb_host,
+    listen_port    => $puppetdb_port
   }
   class { 'puppetdb::master::config':
-    puppetdb_server         => 'localhost',
-    puppetdb_port           => 8081,
+    puppetdb_server         => $puppetdb_host,
+    puppetdb_port           => $puppetdb_port,
     # Manage all the things in the puppet master!
     manage_routes           => true,
     manage_storeconfigs     => true,
-    manage_report_processor => true
+    manage_report_processor => true,
+    enable_reports          => true
   }
 }
